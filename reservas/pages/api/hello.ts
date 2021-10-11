e39520d9699240186a5b2ff300cf558f3123bb82
Wilton Ribeiro <wilton.tecti@gmail.com>
2021-10-11 10:14:05 -0300

    adicionado arquivos do servico de reservas

---
+++ reservas/pages/api/hello.ts
@@ -0,0 +1,24 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import client from '../../infra/grpc'
+import {promisify} from 'util';
+
+const buscarVeiculosParaLocacaoPorPeriodo = promisify(client.buscarVeiculosParaLocacaoPorPeriodo)
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Object>
+) {
+
+  try{
+      client.buscarVeiculosParaLocacaoPorPeriodo({dataInicio : "2021-01-01", dataFim: "2021-01-31", cidade : "Goiânia"}, function(err:any, response:any){
+        if (err)
+          return res.status(500).json({message: "Erro ao conectar no serviço GRPC", err})
+          
+
+        return res.status(200).json({ name: 'John Doe', ...response })
+      })
+    }catch(err){
+    }
+
+}
