99cecadb10bfc8d8a523b43d6c53cb0524744305
willtecti <wilton.tecti@gmail.com>
2021-11-07 12:49:51 -0300

    Finalizado input e integrando servicos

---
+++ locadoras/src/domain/Locadora.ts
@@ -0,0 +1,13 @@
+export default class Locadora{
+
+
+    constructor( private nome:String, private logo: String){}
+
+    getNome(){
+        return this.nome
+    }
+
+    getLogo(){
+        return this.logo
+    }
+}
---
+++ reservas/locadoras/service.ts
@@ -0,0 +1,11 @@
+const urlGatway = "http://localhost:3002"
+
+export interface LocadoraResp { nome: String, logo: String}
+export async function  listarLocadoras(): Promise<[LocadoraResp]>{
+    console.log('listarLocadoras')
+    const resp = await fetch(urlGatway+'/locadoras')
+    const locadora : [LocadoraResp] = await  resp.json()
+    console.log('l',resp, locadora)
+    return locadora
+
+}
